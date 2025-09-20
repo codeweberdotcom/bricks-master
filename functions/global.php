deb8360da0bd007c678e8dae960e440dfaa5075b
Denis Korablev <info@codeweber.com>
2025-09-20 04:00:07 +0300

    fix bug

--- functions/global.php
+++ functions/global.php
@@ -336,9 +336,11 @@ function universal_title($tag = false, $theme = false)
 			$title = get_the_date();
 		} elseif (is_tax()) {
 			$title = single_term_title('', false);
-		} elseif (is_shop() && class_exists('WooCommerce')) {
+		} elseif (is_post_type_archive()) {
+			$title = post_type_archive_title('', false);
+		} elseif (function_exists('is_shop') && is_shop() && class_exists('WooCommerce')) {
 			// Для страницы архива магазина WooCommerce
-			$title = woocommerce_page_title(false);
+			$title = function_exists('woocommerce_page_title') ? woocommerce_page_title(false) : __('Shop', 'codeweber');
 		} else {
 			$title = get_the_archive_title();
 		}
