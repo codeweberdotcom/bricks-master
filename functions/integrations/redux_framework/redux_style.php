27f3e22fc5a181639ff38fce298134eb91536855
Denis Korablev <info@codeweber.com>
2025-12-08 03:24:31 +0300

    Refactor SCSS variables and accordion styles
    - Removed redundant color and breakpoint variables from _user-variables.scss.
    - Consolidated accordion variables into _variables.scss for better organization.
    - Updated accordion styles in _accordion.scss to utilize new variable structure.
    - Improved icon positioning logic for accordion buttons and added support for multiple icon types.
    - Enhanced button class generation in single.php template for better theme integration.

--- functions/integrations/redux_framework/redux_style.php
+++ functions/integrations/redux_framework/redux_style.php
@@ -27,3 +27,31 @@ if (! function_exists('getThemeButton')) {
       return isset($style_map[$style_key]) ? $style_map[$style_key] : $default_class;
    }
 }
+
+/**
+ * Получение стиля скругления карточек и изображений из Redux Framework
+ * Использование: <?php echo getThemeCardImageRadius(); ?> - вернет стандартный Bootstrap класс
+ *
+ * @param string $default_class Класс по умолчанию
+ * @return string Стандартный Bootstrap CSS-класс скругления
+ */
+if (! function_exists('getThemeCardImageRadius')) {
+   function getThemeCardImageRadius($default_class = '')
+   {
+      global $opt_name;
+
+      // Карта соответствий опций Redux → стандартные Bootstrap классы
+      $style_map = [
+         '1' => 'rounded-pill',
+         '2' => '',
+         '3' => 'rounded-xl',
+         '4' => 'rounded-0',
+      ];
+
+      // Получаем значение из Redux (по умолчанию '2')
+      $style_key = Redux::get_option($opt_name, 'opt_card_image_border_radius', '2');
+
+      // Возвращаем стандартный Bootstrap класс
+      return isset($style_map[$style_key]) ? $style_map[$style_key] : $default_class;
+   }
+}
