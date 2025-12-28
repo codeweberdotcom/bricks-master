79ed0c2b7333addc21e6d4fcc716b713a08a0d61
Denis Korablev <info@codeweber.com>
2025-12-28 21:42:06 +0300

    refactor(page.php): streamline content rendering by consolidating the_content and wp_link_pages functions for improved readability

--- templates/content/page.php
+++ templates/content/page.php
@@ -16,20 +16,18 @@
 				</div>
 			</div>
 		<?php } ?>
-		<div>
-			<?php
-			the_content();
+		<?php
+		the_content();
 
-			wp_link_pages(
-				array(
-					'before'        => '<nav class="nav"><span class="nav-link">' . esc_html__('Part:', 'bricks') . '</span>',
-					'after'         => '</nav>',
-					'link_before'   => '<span class="nav-link">',
-					'link_after'    => '</span>',
-				)
-			);
-			?>
-		</div>
+		wp_link_pages(
+			array(
+				'before'        => '<nav class="nav"><span class="nav-link">' . esc_html__('Part:', 'bricks') . '</span>',
+				'after'         => '</nav>',
+				'link_before'   => '<span class="nav-link">',
+				'link_after'    => '</span>',
+			)
+		);
+		?>
 	</article> <!-- #post-<?php the_ID(); ?> -->
 
 <?php } ?>
