6bdd67351efae4ace95395228cf12f19edaf46fc
CodeWeber Dev <dev@codeweber.ru>
2026-05-03 13:19:55 +0200

    fix(documents): exclude from frontend search results

--- functions/cpt/cpt-documents.php
+++ functions/cpt/cpt-documents.php
@@ -43,7 +43,7 @@ function register_my_cpt_documents()
       "show_in_menu" => true,
       "show_in_nav_menus" => true,
       "delete_with_user" => false,
-      "exclude_from_search" => false,
+      "exclude_from_search" => true,
       "capability_type" => "post",
       "map_meta_cap" => true,
       "hierarchical" => true,
